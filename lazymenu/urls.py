68bc206a913809a11d7ce0890d3860177805d8f6
Arturo Bravo rovirosa <arturobravorovirosa@Arturos-MacBook-Air.local>
2022-10-20 22:45:28 -0700

    second

--- lazymenu/urls.py
+++ lazymenu/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from lazymenu_api import urls as lazymenu_urls
 
 urlpatterns = [
+    path('todos/', include(lazymenu_urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('todos/', include(lazymenu_urls)),
 ]
